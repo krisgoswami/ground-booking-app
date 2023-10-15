7b9f64b9bad98ac4fa2dada37510303e19c23603
Krishanu <krishgoswami15@gmail.com>
2023-10-15 19:54:42 +0530

    configured

--- index.js
+++ index.js
@@ -1,2 +1,24 @@
 import express from 'express';
 import cors from 'cors';
+import connectDB from './server/dbconfig/connectdb';
+import adminRoute from './server/routes/adminRoutes.js';
+
+const app = express();
+
+//connect db
+connectDB();
+
+//middlewares
+app.use(cors);
+app.use(express.json());
+
+//routes
+app.use('api/v1/admin', adminRoute);
+
+//port
+const PORT = process.env.PORT || 8080;
+
+//listen 
+app.listen(PORT, () => {
+    console.log(`Server started on port: ${PORT}`);
+});
