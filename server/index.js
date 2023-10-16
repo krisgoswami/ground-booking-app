1dc6995e65b6d3bf5d2d371e9d7b01a0d093f370
Krishanu <krishgoswami15@gmail.com>
2023-10-16 22:38:43 +0530

    uplaod image file middleware added

--- server/index.js
+++ server/index.js
@@ -15,6 +15,9 @@ app.use(express.json());
 //routes
 app.use('/api/v1/admin', adminRoute);
 
+//img uploads
+app.use('uploads/', express.static('uploads'));
+
 //port
 const PORT = process.env.PORT || 8080;
 
