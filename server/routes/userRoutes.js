44232a2c9c6b32848d2368907cdbf20aa05b499f
Krishanu <krishgoswami15@gmail.com>
2023-10-18 15:43:19 +0530

    endpoints created

--- server/routes/userRoutes.js
+++ server/routes/userRoutes.js
@@ -1,5 +1,5 @@
 import express from "express";
-import { getGroundById, getGrounds, userLogin, userSignup } from "../controllers/userController.js";
+import { bookTimeSlot, getGroundById, getGrounds, userLogin, userSignup } from "../controllers/userController.js";
 import { authenticateJWT } from "../utils/jwtAuth.js";
 
 const router = express.Router();
@@ -8,6 +8,7 @@ router.post('/signup', userSignup);
 router.post('/login', userLogin);
 router.get('/grounds', getGrounds);
 router.get('/ground/:id', authenticateJWT, getGroundById);
+router.post('/book-slot', authenticateJWT, bookTimeSlot);
 
 
 export default router;
