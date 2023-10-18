e774efbaac5681cbfaaa1901c930c99ed7eb4188
Krishanu <krishgoswami15@gmail.com>
2023-10-18 15:04:58 +0530

    routes added

--- server/routes/userRoutes.js
+++ server/routes/userRoutes.js
@@ -1,9 +1,13 @@
 import express from "express";
-import { userLogin, userSignup } from "../controllers/userController.js";
+import { getGroundById, getGrounds, userLogin, userSignup } from "../controllers/userController.js";
+import { authenticateJWT } from "../utils/jwtAuth.js";
 
 const router = express.Router();
 
 router.post('/signup', userSignup);
 router.post('/login', userLogin);
+router.get('/grounds', getGrounds);
+router.get('/ground/:id', authenticateJWT, getGroundById);
+
 
 export default router;
