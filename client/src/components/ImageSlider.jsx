2de4db03862ec98693f94af1a2f6f9a5534ceac9
Krishanu <krishgoswami15@gmail.com>
2023-10-21 23:05:19 +0530

    modified

--- client/src/components/ImageSlider.jsx
+++ client/src/components/ImageSlider.jsx
@@ -1,14 +1,23 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 
 const ImageSlider = () => {
     const images = [
-        'image1.jpg',
-        'image2.jpg',
-        'image3.jpg',
+        'versus 1',
+        'versus 2',
+        'versus 3',
+        'versus 4',
         // Add more image URLs as needed
     ];
 
     const [currentImageIndex, setCurrentImageIndex] = useState(0);
+
+    useEffect(() => {
+        const interval = setInterval(() => {
+            setCurrentImageIndex((prevIndex) => (prevIndex === images.length - 1 ? 0 : prevIndex + 1));
+        }, 3000); // Change image every 5 seconds
+
+        return () => clearInterval(interval);
+    }, []);
 
     const handlePrev = () => {
         setCurrentImageIndex((prevIndex) => (prevIndex === 0 ? images.length - 1 : prevIndex - 1));
@@ -19,21 +28,26 @@ const ImageSlider = () => {
     };
 
     return (
-        <div className="relative bg-black opacity-80 text-white w-full h-96 overflow-hidden">
+        <div className="relative text-white w-full h-screen overflow-hidden">
+            <div className="absolute inset-0 bg-black opacity-60 z-20"></div>
+            <div className="absolute inset-0 flex items-center justify-center z-30">
+                <h1 className="text-white text-4xl font-bold">Welcome to Ground Booking App</h1>
+            </div>
+
             {images.map((image, index) => (
                 <img
                     key={index}
-                    src={''}
+                    src={`../src/images/${image}.jpg`}
                     alt={`Image ${index + 1}`}
-                    className={`absolute top-0 left-0 w-full h-full transition-opacity duration-500 ${index === currentImageIndex ? 'opacity-100' : 'opacity-0'}`}
+                    className={`absolute top-0 left-0 w-full h-full object-cover transition-opacity duration-500 ${index === currentImageIndex ? 'opacity-100' : 'opacity-0'}`}
                 />
             ))}
-            <button onClick={handlePrev} className="absolute top-1/2 left-4 transform -translate-y-1/2 bg-black bg-opacity-50 text-white px-4 py-2 rounded-full focus:outline-none">
+            {/* <button onClick={handlePrev} className="absolute top-1/2 left-4 transform -translate-y-1/2 bg-black bg-opacity-50 text-white px-4 py-2 rounded-full focus:outline-none">
                 Prev
             </button>
             <button onClick={handleNext} className="absolute top-1/2 right-4 transform -translate-y-1/2 bg-black bg-opacity-50 text-white px-4 py-2 rounded-full focus:outline-none">
                 Next
-            </button>
+            </button> */}
         </div>
     );
 };
