68b36d9757f3f8ac8798d275158d4a940968d7ee
Krishanu <krishgoswami15@gmail.com>
2023-10-23 22:38:13 +0530

    modified

--- admin/src/App.jsx
+++ admin/src/App.jsx
@@ -1,13 +1,25 @@
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import './App.css'
 import Navbar from './components/Navbar'
 import Landing from './pages/Landing'
+import Login from './pages/Login'
+import { Provider } from 'react-redux';
+import { Toaster } from 'react-hot-toast';
+import { store } from './redux/store';
 
 function App() {
 
   return (
-    <>
-      <Landing />
-    </>
+    <Provider store={store}>
+      <Toaster />
+      <Router>
+        <Routes>
+          <Route path="/landing" element={<Landing />} />
+          <Route path="/" element={<Login />} />
+          {/* <Route path="/signup" element={<Signup />} /> */}
+        </Routes>
+      </Router>
+    </Provider>
   )
 }
 
--- admin/src/components/SidePanel.jsx
+++ admin/src/components/SidePanel.jsx
@@ -1,10 +1,39 @@
 // src/components/SidePanel.js
 
 import React from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+import { useNavigate } from 'react-router-dom';
+import { authActions } from '../redux/store';
+import toast from 'react-hot-toast';
 
 const SidePanel = () => {
+
+    //global state
+    let isLogin = useSelector((state) => state.isLogin);
+    isLogin = isLogin || localStorage.getItem('userId');
+
+    let user = localStorage.getItem("email");
+
+    const dispatch = useDispatch();
+    const navigate = useNavigate();
+
+    //handle logout
+    const handleLogout = () => {
+        try {
+            dispatch(authActions.logout());
+            localStorage.clear();
+            toast("You've been logged out", {
+                icon: '⚠️',
+            });
+            navigate('/');
+        } catch (error) {
+            console.log(error);
+        }
+    }
+
+
     return (
-        <div className="flex flex-col justify-between fixed inset-y-0 left-0 w-64 bg-blue-500 text-white p-4">
+        <div className="flex flex-col justify-between fixed inset-y-0 left-0 w-64 bg-green-700 text-white p-4">
             <div className="flex items-center mb-8">
                 <h2 className="text-2xl font-bold">Ground Booking</h2>
             </div>
@@ -20,7 +49,7 @@ const SidePanel = () => {
                 </li>
             </ul>
             <div className="mt-auto mb-5">
-                <button className="bg-white text-blue-500 py-2 px-4 rounded-full block w-full mb-2">Logout</button>
+                <button onClick={handleLogout} className="bg-white text-green-700 font-bold py-2 px-4 rounded-full block w-full mb-2">Logout</button>
             </div>
         </div>
     );
