b569de78b4f69f6c4480c14a912e116a8f323aea
Krishanu <krishgoswami15@gmail.com>
2023-10-25 00:35:06 +0530

    done

--- admin/src/pages/CreateGround.jsx
+++ admin/src/pages/CreateGround.jsx
@@ -1,8 +1,166 @@
 import React from 'react'
+import { useState } from 'react';
+import { useNavigate } from 'react-router-dom';
+import axios from 'axios';
+import { BASE_URL } from '../utils/helper';
+import Switch from 'react-switch';
+import toast from 'react-hot-toast';
+
 
 const CreateGround = () => {
+
+    const navigate = useNavigate();
+    const token = localStorage.getItem('token');
+
+    const [inputs, setInputs] = useState({
+        ground_name: "",
+        location: "",
+        description: "",
+        price: "",
+        published: false,
+        images: [],
+    });
+
+    //handle publish switch change
+    const handleSwitchChange = () => {
+        setInputs(prevState => ({
+            ...prevState,
+            published: !prevState.published
+        }))
+    }
+
+    //handle input change
+    const handleInputChange = (e) => {
+        setInputs(prevState => ({
+            ...prevState,
+            [e.target.name]: e.target.value,
+        }));
+    }
+
+    const handleFileChange = (e) => {
+        const files = Array.from(e.target.files);
+        setInputs({ ...inputs, images: files });
+    };
+
+    const handleSubmit = async (e) => {
+        e.preventDefault();
+        if (!inputs.ground_name || !inputs.description || !inputs.price || !inputs.location) {
+            toast.error("Fields cannot be empty");
+            return;
+        }
+
+        try {
+            if (token) {
+                const formData = new FormData(); // Create a FormData object to send files
+
+                // Append other form data fields
+                formData.append('ground_name', inputs.ground_name);
+                formData.append('description', inputs.description);
+                formData.append('price', inputs.price);
+                formData.append('location', inputs.location);
+                formData.append('published', inputs.published);
+
+                // Append the image files
+                for (let i = 0; i < inputs.images.length; i++) {
+                    formData.append(`images`, inputs.images[i]);
+                }
+
+                const { data } = await axios.post(`${BASE_URL}/api/v1/admin/create-ground`, formData, {
+                    headers: {
+                        'Authorization': `Bearer ${token}`,
+                        'Content-Type': 'multipart/form-data', // Set content type to multipart form data
+                    }
+                });
+                if (data.success) {
+                    toast.success("Ground created");
+                    // navigate('/all-courses');
+                } else {
+                    toast.error("Something went wrong");
+                }
+            }
+        } catch (error) {
+            console.log(error);
+        }
+    }
+
     return (
-        <div>CreateGround</div>
+        <div className="p-8">
+            <h2 className="text-2xl font-bold mb-4">Create Ground</h2>
+            <form onSubmit={handleSubmit}>
+                <div className="mb-4">
+                    <label className="block text-gray-700 mb-2">Ground Name</label>
+                    <input
+                        type="text"
+                        name="ground_name"
+                        value={inputs.ground_name}
+                        onChange={handleInputChange}
+                        className="w-full border border-gray-300 rounded px-4 py-2"
+                    />
+                </div>
+                <div className="mb-4">
+                    <label className="block text-gray-700 mb-2">Location</label>
+                    <input
+                        type="text"
+                        name="location"
+                        value={inputs.location}
+                        onChange={handleInputChange}
+                        className="w-full border border-gray-300 rounded px-4 py-2"
+                    />
+                </div>
+                <div className="mb-4">
+                    <label className="block text-gray-700 mb-2">Description</label>
+                    <textarea
+                        name="description"
+                        value={inputs.description}
+                        onChange={handleInputChange}
+                        className="w-full border border-gray-300 rounded px-4 py-2 h-32"
+                    />
+                </div>
+                <div className="mb-4">
+                    <label className="block text-gray-700 mb-2">Price</label>
+                    <input
+                        type="text"
+                        name="price"
+                        value={inputs.price}
+                        onChange={handleInputChange}
+                        className="w-full border border-gray-300 rounded px-4 py-2"
+                    />
+                </div>
+                <div className="mb-4">
+                    <label className="block text-gray-700 mb-2">Published</label>
+                    <Switch
+                        onChange={handleSwitchChange}
+                        checked={inputs.published}
+                        onColor="#86d3ff"
+                        onHandleColor="#2693e6"
+                        handleDiameter={30}
+                        uncheckedIcon={false}
+                        checkedIcon={false}
+                        boxShadow="0px 1px 5px rgba(0, 0, 0, 0.6)"
+                        activeBoxShadow="0px 0px 1px 10px rgba(0, 0, 0, 0.2)"
+                        height={20}
+                        width={48}
+                        className="react-switch"
+                    />
+                </div>
+                <div className="mb-4">
+                    <label className="block text-gray-700 mb-2">Images</label>
+                    <input
+                        type="file"
+                        accept="image/*"
+                        multiple
+                        onChange={handleFileChange}
+                        className="w-full border border-gray-300 rounded px-4 py-2"
+                    />
+                </div>
+                <button
+                    type="submit"
+                    className="bg-green-700 hover:bg-green-900 text-white py-2 px-4 rounded-full"
+                >
+                    Create Ground
+                </button>
+            </form>
+        </div>
     )
 }
 
