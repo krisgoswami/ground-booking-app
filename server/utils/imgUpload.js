a4c6b4b09872e0c900f266b655d9d68f278e898f
Krishanu <krishgoswami15@gmail.com>
2023-10-25 18:21:41 +0530

    modified

--- server/utils/imgUpload.js
+++ server/utils/imgUpload.js
@@ -5,7 +5,7 @@ const storage = multer.diskStorage({
         callback(null, 'uploads/');
     },
     filename: (req, file, callback) => {
-        callback(null, Date.now() + "-" + file.originalname);
+        callback(null, file.originalname);
     },
 });
 
