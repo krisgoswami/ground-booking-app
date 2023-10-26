bc046bf7904cd45e412ab8096097df3e08f3c4ba
Krishanu <krishgoswami15@gmail.com>
2023-10-26 15:55:20 +0530

    router added

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,17 +1,29 @@
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import './App.css'
 import Navbar from './components/Navbar'
 import Landing from './pages/Landing'
 import Login from './pages/Login'
 import Signup from './pages/Signup'
+import { Provider } from 'react-redux';
+import { Toaster } from 'react-hot-toast';
+import { store } from './redux/store';
 
 function App() {
 
   return (
     <>
-      <Navbar />
-      <Landing />
-      {/* <Login /> */}
-      {/* <Signup /> */}
+      <Provider store={store}>
+        <Toaster />
+        <Router>
+          <Routes>
+            <Route path="/login" element={<Login />} />
+            <Route path="/signup" element={<Signup />} />
+            <Route path="/home" exact element={<Landing />} />
+          </Routes>
+        </Router>
+        <Navbar />
+        <Landing />2
+      </Provider>
     </>
   )
 }
