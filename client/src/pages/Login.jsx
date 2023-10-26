23f0effcf6cc65780d9902a9f604761662d175e2
Krishanu <krishgoswami15@gmail.com>
2023-10-26 18:12:56 +0530

    both auth in one page

--- client/src/pages/Login.jsx
+++ client/src/pages/Login.jsx
@@ -1,13 +1,29 @@
 import React, { useState } from 'react';
-import toast from 'react-hot-toast';
 import axios from 'axios';
+import toast from 'react-hot-toast';
+import { useNavigate } from 'react-router-dom';
+import { useDispatch } from "react-redux";
+import { authActions } from "../redux/store";
 import { BASE_URL } from '../utils/helper';
+import { useCallback } from 'react';
 
 const Login = () => {
+
+    const navigate = useNavigate();
+    const dispatch = useDispatch();
+
+    const [authState, setAuthState] = useState('login');
     const [inputs, setInputs] = useState({
+        username: '',
         email: '',
         password: '',
     });
+
+    //toggle between login and signup
+
+    const toggleAuthState = useCallback(() => {
+        setAuthState((currentAuthState) => currentAuthState === "login" ? "signup" : "login");
+    }, []);
 
     // handle input change
     const handleOnChange = (e) => {
@@ -18,7 +34,7 @@ const Login = () => {
     }
 
     //login logic
-    const handleOnSubmit = async (e) => {
+    const handleLogin = async (e) => {
         e.preventDefault();
         if (!inputs.email || !inputs.password) {
             toast.error("Fields cannot be empty");
@@ -26,7 +42,7 @@ const Login = () => {
         }
 
         try {
-            const { data } = await axios.post(`${BASE_URL}/api/v1/user/login`, {
+            const { data } = await axios.post(`${BASE_URL}/api/v1/admin/login`, {
                 email: inputs.email,
                 password: inputs.password,
             });
@@ -34,66 +50,124 @@ const Login = () => {
                 localStorage.setItem('token', data.token);
                 localStorage.setItem("userId", data?.admin._id);
                 localStorage.setItem("email", data?.admin.email);
-                // dispatch(authActions.login());
+                localStorage.setItem("username", data?.admin.username);
+                dispatch(authActions.login());
                 toast.success("Logged in");
-                // navigate('/all-courses');
+                navigate('/home');
             } else {
                 toast.error("Email or password incorrect");
             }
         } catch (error) {
             console.log(error);
-            toast.error("Email or password incorrect");
+            toast.error("incorrect");
+        }
+    };
+    //signup logic
+    const handleSignup = async (e) => {
+        e.preventDefault();
+        if (!inputs.username || !inputs.email || !inputs.password) {
+            toast.error("Fields cannot be empty");
+            return;
+        }
+
+        try {
+            const { data } = await axios.post(`${BASE_URL}/api/v1/admin/signup`, {
+                username: inputs.username,
+                email: inputs.email,
+                password: inputs.password,
+            });
+            if (data.success) {
+                localStorage.setItem('token', data.token);
+                localStorage.setItem("userId", data?.admin._id);
+                localStorage.setItem("email", data?.admin.email);
+                localStorage.setItem("username", data?.admin.username);
+                dispatch(authActions.login());
+                toast.success("Registered");
+                navigate('/home');
+            } else {
+                toast.error("Email or password incorrect");
+            }
+        } catch (error) {
+            console.log(error);
+            toast.error("incorrect");
         }
     };
 
     return (
-        <div className="flex justify-center items-center h-screen">
-            <form className="bg-white shadow-lg rounded px-8 pt-6 pb-8 mb-4 w-full max-w-md" onSubmit={handleOnSubmit}>
-                <div className="mb-4">
-                    <label className="block text-gray-700 text-sm font-bold mb-2" htmlFor="email">
-                        Email
-                    </label>
-                    <input
-                        className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
-                        id="email"
-                        type="email"
-                        placeholder="Email"
-                        name="email"
-                        value={inputs.email}
-                        onChange={handleOnChange}
-                        required
-                    />
+        <div className="flex h-screen">
+            {/* Left side with image and text */}
+            <div className="flex-1 bg-green-700 text-white p-12 flex items-center justify-center">
+                <div>
+                    <h2 className="text-4xl font-bold mb-4">Welcome to Ground Booking App</h2>
+                    <p className="text-lg">Login to access your account.</p>
                 </div>
-                <div className="mb-6">
-                    <label className="block text-gray-700 text-sm font-bold mb-2" htmlFor="password">
-                        Password
-                    </label>
-                    <input
-                        className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
-                        id="password"
-                        type="password"
-                        placeholder="Password"
-                        name="password"
-                        value={inputs.password}
-                        onChange={handleOnChange}
-                        required
-                    />
-                </div>
-                <div className="flex items-center justify-between">
-                    <button
-                        className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline"
+            </div>
+
+            {/* Right side with login form */}
+            <div className="flex-1 flex items-center justify-center p-12">
+                <form className="max-w-md w-full space-y-4">
+                    <h2 className="text-2xl font-bold text-center">
+                        {authState === "login" ? "Login" : "Create an account"}
+                    </h2>
+
+                    {authState === "signup" && (
+                        <div>
+                            <label className="block text-gray-700">Username</label>
+                            <input
+                                type="text"
+                                name="username"
+                                className="w-full border border-gray-300 rounded px-4 py-2"
+                                placeholder="Enter your Username"
+                                value={inputs.username}
+                                onChange={handleOnChange}
+                            />
+                        </div>
+                    )}
+                    <div>
+                        <label className="block text-gray-700">Email</label>
+                        <input
+                            type="email"
+                            name="email"
+                            className="w-full border border-gray-300 rounded px-4 py-2"
+                            placeholder="Enter your email"
+                            value={inputs.email}
+                            onChange={handleOnChange}
+                        />
+                    </div>
+                    <div>
+                        <label className="block text-gray-700">Password</label>
+                        <input
+                            type="password"
+                            name="password"
+                            className="w-full border border-gray-300 rounded px-4 py-2"
+                            placeholder="Enter your password"
+                            value={inputs.password}
+                            onChange={handleOnChange}
+                        />
+                    </div>
+                    {authState === "login" ? <button
                         type="submit"
+                        className="w-full bg-green-700 hover:bg-green-900 text-white py-2 rounded-lg"
+                        onClick={handleLogin}
                     >
                         Login
-                    </button>
-                    <a className="inline-block align-baseline font-bold text-sm text-blue-500 hover:text-blue-800" href="#">
-                        Forgot Password?
-                    </a>
-                </div>
-            </form>
+                    </button> : <button
+                        type="submit"
+                        className="w-full bg-green-700 hover:bg-green-900 text-white py-2 rounded-lg"
+                        onClick={handleSignup}
+                    >
+                        Sign Up
+                    </button>}
+
+                    <p>{authState === "login" ? "Don't have an account?" : "Already have an account?"}
+                        <span>{authState === "login" ?
+                            <a className='ml-1 hover:underline cursor-pointer text-green-700' onClick={toggleAuthState}>Sign Up</a> : <a className='ml-1 hover:underline cursor-pointer text-green-700' onClick={toggleAuthState}>Login</a>}
+                        </span>
+                    </p>
+                </form>
+            </div>
         </div>
     );
 };
-
 
 export default Login;
