0887f928e29c5c776b82bb5fb264ced3af94dc2b
Krishanu <krishgoswami15@gmail.com>
2023-10-30 19:09:06 +0530

    mod

--- admin/src/pages/Landing.jsx
+++ admin/src/pages/Landing.jsx
@@ -1,12 +1,18 @@
 import React from 'react'
 import SidePanel from '../components/SidePanel'
 import Grounds from './Grounds'
+import Bookings from './Bookings'
 
 const Landing = () => {
     return (
         <div>
-            <SidePanel />
-            <Grounds />
+            <p className=' ml-10 mt-5 mb-3 font-bold text-xl p-8'>Listed Grounds</p>
+            <div className='flex flex-wrap justify-start gap-5'>
+                <Grounds />
+            </div>
+            <div className='flex flex-wrap justify-start gap-5'>
+                <Bookings />
+            </div>
         </div>
     )
 }
